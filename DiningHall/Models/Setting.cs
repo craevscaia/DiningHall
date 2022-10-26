52eb3c5a21249ef81aebc14e2aa8e10c27800816
craevscaia <valiushacraevscaia@gmail.com>
2022-10-26 13:11:18 +0300

    Restaurant
    Restaurant

--- DiningHall/Models/Setting.cs
+++ DiningHall/Models/Setting.cs
@@ -8,4 +8,5 @@ public static class Setting
     
     public const string KitchenUrl = "https://localhost:7166/kitchen";
     public const string FoodOrderingServiceRegisterUrl = "https://localhost:7143/register";
+    // public const string FoodOrderingServiceRegisterUrl = "http://host.docker.internal:7143/register";
 }
